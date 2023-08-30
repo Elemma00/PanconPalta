96a8aaa670c23edabd2677a3ba4ddb65bfef34f7
Elemma00 <faundez76@gmail.com>
2023-08-29 22:17:27 -0400

    cambios en procesamiento de movimientos

--- scenes/player.gd
+++ scenes/player.gd
@@ -2,7 +2,7 @@ class_name Player
 extends CharacterBody2D
 
 var max_speed = 100
-var jump_speed = 500
+var jump_speed = 300
 var acceleration = 3000
 var gravity = 1000
 
@@ -16,21 +16,21 @@ func _ready() -> void:
 
 
 func _physics_process(delta: float) -> void:
+	if not is_on_floor():
+		velocity.y += gravity * delta
+	
 	if is_multiplayer_authority():
 		var move_input = Input.get_axis("move_left", "move_right")
-		if not is_on_floor():
-			velocity.y += gravity * delta
 		
 		if Input.is_action_just_pressed("jump"):
-			velocity.y = -jump_speed
+			jump.rpc()
 	
 		velocity.x = move_toward(velocity.x, max_speed * move_input, acceleration * delta)
-		move_and_slide()
-		send_info.rpc(global_position)
+		send_info.rpc(global_position,velocity)
 		
 		# Animation
 		if velocity.x != 0:
-			sprite_2d.scale.x = sign(velocity.x)
+			sign_sprite.rpc()
 		
 		if is_on_floor():
 			if abs(velocity.x) > 10 or move_input:
@@ -38,8 +38,10 @@ func _physics_process(delta: float) -> void:
 			else:
 				playback.travel("idle")
 		
-#	else:
-#		pass
+		
+	#movemos el move and slide afuera para poder simular los movimientos
+	#tanto en el servidor como en un cliente
+	move_and_slide()
 
 
 func _input(event: InputEvent) -> void:
@@ -49,8 +51,20 @@ func _input(event: InputEvent) -> void:
 
 
 @rpc("unreliable_ordered")
-func send_info(pos: Vector2) -> void:
-	global_position = pos
+func send_info(pos: Vector2, vel:Vector2) -> void:
+	#interpolamos la posicion y la velocidad  del personaje 
+	global_position = lerp(global_position, pos, 0.5)
+	velocity = lerp(velocity,vel,0.5)
+
+
+#funcion para enviar info de hacia donde mira el player
+@rpc("call_local","reliable")
+func sign_sprite():
+	sprite_2d.scale.x = sign(velocity.x)
+
+@rpc("call_local","reliable")
+func jump():
+	velocity.y = -jump_speed
 
 func setup(player_data: Game.PlayerData):
 	set_multiplayer_authority(player_data.id)
