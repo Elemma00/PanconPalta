ab1dba788058cddbe4f625c143b22a3b286a2dfe
Elemma00 <faundez76@gmail.com>
2023-10-05 17:30:44 -0300

    fix caja

--- scenes/players/playerC.gd
+++ scenes/players/playerC.gd
@@ -1,5 +1,6 @@
 extends Player
 @onready var drop_zone = $drop_zone
+@onready var marker_2d = $Marker2D
 
 @rpc("call_remote")
 func sign_drop():
@@ -7,3 +8,6 @@ func sign_drop():
 	var lastposition = drop_zone.position.x 
 	drop_zone.position.x = lastposition * lastDir
 
+@rpc("call_local","reliable","any_peer")
+func setremotepath(value):
+	marker_2d.remote_path = value
--- scripts/objects/caja_1.gd
+++ scripts/objects/caja_1.gd
@@ -1,28 +1,36 @@
 extends RigidBody2D
 
-var picked = false
+
+@export var picked = false: 
+	set(value):
+		picked = value
+		freeze = picked
+
 var playerC
 @onready var caja_1 = $"."
 @onready var collision_shape_2d = $CollisionShape2D
 
-
+"
 func _physics_process(delta):
-	if is_multiplayer_authority():
-		position_info.rpc()
-
+	position_info()
+"
 func _input(event):
 	if Input.is_action_just_pressed("skill") and picked == false:
+		Debug.dprint("aaaa h")
 		var bodies = $Area2D.get_overlapping_bodies()
 		for body in bodies:
 			if body.is_in_group("playerC"):
 				playerC = body
 				if body.canPick == true:
-					picked = true
+					playerC.setremotepath.rpc(get_path())
+					Debug.dprint("pick")
+					request_pick.rpc_id(1,true)
 					body.canPick = false
 
 	elif Input.is_action_just_pressed("drop") and picked == true:
-		picked = false
+		request_pick.rpc_id(1,false)
 		playerC.canPick = true
+		playerC.setremotepath.rpc("")
 		drop.rpc()
 
 
@@ -38,5 +46,8 @@ func position_info():
 @rpc("reliable","call_local")
 func drop():
 	var drop_zone = playerC.get_node("drop_zone")
-	self.position = drop_zone.global_position
+'	self.position = drop_zone.global_position'
 	
+@rpc("reliable","any_peer","call_local")
+func request_pick(value):
+	picked = value
