866e4e0c32a7c2259c5899254030ac1c782ffd5b
cms1598 <cms1598@gmail.com>
2023-10-18 22:34:52 -0300

    feat: add new version of level

--- scripts/objects/spikes.gd
+++ scripts/objects/spikes.gd
@@ -1,17 +1,20 @@
 extends Area2D
 
+
 func _ready():
 	pass
-	
-func _on_body_entered(area:Node2D) -> void:
-	if area.is_in_group("player"):
-		print("lolaso")
-		
 
-#@rpc("call_remote","reliable")
-#func _fail():
-#	var current_scene = get_tree().get_current_scene()
-#	var new_scene = load(current_scene.filename).instance()
-#	get_tree().root.add_child(new_scene)
-#	get_tree().set_current_scene(new_scene)
-#	current_scene.queue_free()
+func _on_body_entered(body: Node2D) -> void:
+	if !body.is_in_group("players"):
+		return
+	
+	if !is_multiplayer_authority():
+		return
+	
+	print("Murio")
+	
+	_restart_level.rpc()
+
+@rpc("call_local","reliable")
+func _restart_level() -> void:
+	get_tree().reload_current_scene()
