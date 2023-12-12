4ee18299e58dac24980c4c733cbdeb44d8473181
cms1598 <cms1598@gmail.com>
2023-12-12 19:51:07 -0300

    feat: add background to sections

--- scenes/levels/key.gd
+++ scenes/levels/key.gd
@@ -1,7 +1,8 @@
 extends Area2D
 
 @onready var collision: CollisionShape2D = $CollisionShape2D
-@onready var icon: AnimatedSprite2D = $AnimatedSprite2D
+@onready var asprite: AnimatedSprite2D = $AnimatedSprite2D
+@onready var icon: Sprite2D = $Sprite2D
 
 func _on_body_entered(body: Node2D) -> void:
 	if !body.is_in_group("players"):
@@ -15,5 +16,10 @@ func _on_body_entered(body: Node2D) -> void:
 	# Llamar a la función en el nodo global
 	get_parent().incrementar_contador()
 	# Destruir la llave
+	destroy_key.rpc()
+
+
+@rpc("call_local", "reliable")
+func destroy_key():
 	collision.set_disabled(true)
 	icon.hide()
